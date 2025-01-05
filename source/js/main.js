ae3ed6020a4fcec2b941dbac4007ea88b3de08a1
Oksana Lanovenko <okslan@gmail.com>
2025-01-05 20:55:31 +0400

    05/01 (2)

--- source/js/main.js
+++ source/js/main.js
@@ -1,20 +1,23 @@
 // https://swiperjs.com/get-started#installation
 import Swiper from "swiper";
-import {Navigation, Pagination} from "swiper/modules";
+import {Navigation} from "swiper/modules";
 import '../sass/vendor/swiper.scss';
 
-const swiper = new Swiper('.juri__list', {
-    modules: [Navigation, Pagination],
-    // Optional parameters
+/* свайпер блока JURI*/
+const juriSwiper = new Swiper('.juri__swiper', {
+    modules: [Navigation],
     direction: 'horizontal',
     loop: true,
-    spaceBetween: 40,
+    speed: 500,
+
     breakpoints: {
         1365: {
           slidesPerView: 4,
+          spaceBetween: 40,
         },
         767: {
           slidesPerView: 2,
+          spaceBetween: 40,
         },
         320: {
           slidesPerView: 1,
@@ -22,31 +25,18 @@ const swiper = new Swiper('.juri__list', {
 
       },
 
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-      enabled: true,
-      type: 'bullets',
-    },
-
-    // Navigation arrows
+  // Navigation arrows
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
       enabled: true,
       clickable: true,
     },
-
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-      draggable: true,
-    },
   });
 
-  swiper.init();
+  juriSwiper.init();
 
+/* свайпер блока REVIEWS*/
   const swiper_reviews = new Swiper('.reviews__list', {
     modules: [Navigation, Pagination],
     // Optional parameters
@@ -54,7 +44,7 @@ const swiper = new Swiper('.juri__list', {
     loop: true,
     spaceBetween: 40,
     slidesPerView: 1,
-    
+
     // If we need pagination
     pagination: {
       el: '.swiper-pagination',
